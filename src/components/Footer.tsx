c5a9a24b24370d224f10f4cef8134c4288e1206f
alex.barro <a.barro.sendros@gmail.com>
2024-09-23 20:47:23 +0200

    fix: ensure display footer correctly for mobile screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,12 @@ const Footer = () => {
     <footer className="bg-secondary bg-opacity-30 text-light">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center space-x-6">
-          <nav className="flex gap-10 font-medium text-sm">
+          <nav className="flex flex-col sm:flex-row gap-5 sm:gap-10 font-medium text-sm">
             <Link href="/get-started">Get started</Link>
             <Link href="/how-it-works">How does it works?</Link>
             <Link href="/about-us">About us</Link>
             <Link href="/terms-of-service">Terms of Service</Link>
             <Link href="/privacy-policy">Privacy Policy</Link>
-            <Link href="/contact-us">Contact us</Link>
           </nav>
         </div>
         <div className="mt-8 text-center text-sm">
