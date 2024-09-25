76bfdea7523664ce4df40cd3c9cfd3d07c1c372e
alex.barro <a.barro.sendros@gmail.com>
2024-09-25 21:29:13 +0200

    refactor: remove card to manage session from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,5 @@
-"use client"
-import { useSession, signIn, signOut } from "next-auth/react"
-import Button from "@components/Button"
 import Card from "@components/Card"
 
 export default function Home() {
-  const { data: session } = useSession()
-
-  const copyToClipboardToken = () => {
-    navigator.clipboard.writeText(session?.user?.accessToken || "")
-  }
-
-  if (session) {
-    return (
-      <Card>
-        <span>Signed in as {session?.user?.email || session?.user?.name}</span>
-        <div className="flex flex-row gap-2">
-          <Button variant="primary" onClick={copyToClipboardToken}>
-            Copy access token
-          </Button>
-          <Button variant="ghost" onClick={() => signOut()}>
-            Sign out
-          </Button>
-        </div>
-      </Card>
-    )
-  }
-
-  return (
-    <Card>
-      <span>Not signed in</span>
-      <Button
-        variant="secondary"
-        onClick={() => signIn("spotify", { callbackUrl: "/" })}
-      >
-        Login with Spotify
-      </Button>
-    </Card>
-  )
+  return <h1 className="text-xl">Home page</h1>
 }
