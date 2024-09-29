216f582b71da790798b680e0f442e97e3e903c86
alex.barro <a.barro.sendros@gmail.com>
2024-09-29 19:11:05 +0200

    fix: ensure to display UserMenu in mobile screen

--- src/components/layout/Header/NavbarMenu/NavbarMenu.tsx
+++ src/components/layout/Header/NavbarMenu/NavbarMenu.tsx
@@ -1,13 +1,28 @@
 import { Link } from "lucide-react"
 import MobileNavbarMenu from "./MobileNavbarMenu"
 import DesktopNavbarMenu from "./DesktopNavbarMenu"
+import { signIn, useSession } from "next-auth/react"
+import Button from "@/components/ui/Button"
+import UserMenu from "./UserMenu"
 
 const NavbarMenu = () => {
+  const { data: session } = useSession()
+
   return (
-    <>
+    <div className="flex items-center justify-end gap-3 sm:gap-6">
       <MobileNavbarMenu />
       <DesktopNavbarMenu />
-    </>
+      {session ? (
+        <UserMenu session={session} />
+      ) : (
+        <Button
+          variant="primary"
+          onClick={() => signIn("spotify", { callbackUrl: "/" })}
+        >
+          Login with Spotify
+        </Button>
+      )}
+    </div>
   )
 }
 
