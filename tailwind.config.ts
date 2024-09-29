fdbe68c3fe2cf54f7dba44f2c896bd32666b3479
alex.barro <a.barro.sendros@gmail.com>
2024-09-29 17:58:03 +0200

    refactor: use shadcn components instead of radix-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,29 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#0D1B2A",
-        secondary: "#E0E1DD",
-        tertiary: "#778DA9",
-        dark: "#0D1B2A",
-        light: "#778DA9",
-        spotify: "#00DA5A",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#0D1B2A',
+  			secondary: '#E0E1DD',
+  			tertiary: '#778DA9',
+  			dark: '#0D1B2A',
+  			light: '#778DA9',
+  			spotify: '#00DA5A'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
