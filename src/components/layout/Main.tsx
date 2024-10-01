95e1d993fd59bfbb09cbc5bdd70572d150b64f68
alex.barro <a.barro.sendros@gmail.com>
2024-10-01 14:23:27 +0200

    fix: add padding in main container

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,7 +1,9 @@
 const Main = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <main className="flex-grow">
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
     </main>
   )
 }
