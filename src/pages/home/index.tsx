792c01f60cc6f6ab5c146b9c0d8e57542fe4c6ea
alex.barro <a.barro.sendros@gmail.com>
2024-10-02 08:30:39 +0200

    feat: improve SEO in home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head"
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | Festwrap</title>
+        <meta
+          name="description"
+          content="Spotify playlists Generator using festival line-up"
+        />
+        <meta
+          name="keywords"
+          content="spotify, generator, playlist, festival, line-up"
+        />
+        <meta property="og:title" content="Home" />
+        <meta
+          property="og:description"
+          content="Descripción para redes sociales."
+        />
+        <meta property="og:image" content="/path-to-image.jpg" />
+        <meta property="og:url" content="https://tusitio.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>{" "}
+      <div className="flex flex-col">
+        <h1>Home page</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-  return <h1 className="text-xl">Home page</h1>
-}
+import HomePage from "./home"
+
+export default HomePage
