46b3bb253b28af3f7f349b00ee8942c4eac2d31a
alex.barro <a.barro.sendros@gmail.com>
2024-10-05 10:41:42 +0200

    feat(home): create AnimatedSection component

--- src/pages/home/components/HowItWorksSection/HowItWorksSection.tsx
+++ src/pages/home/components/HowItWorksSection/HowItWorksSection.tsx
@@ -1,11 +1,10 @@
 import { motion } from "framer-motion"
-import Heading from "@/components/Heading"
-import Image from "next/image"
-import darkSilverShapeImage from "@public/dark-silver-shape.svg"
-import platinumShapeImage from "@public/platinum-shape.svg"
+import Heading from "@/components/ui/Heading"
+import Image, { StaticImageData } from "next/image"
 import customizePlaylistImage from "@public/customize-playlist.svg"
 import spotifyLockImage from "@public/spotify-lock.svg"
 import searchArtistsImage from "@public/search-artists.svg"
+import AnimatedSection from "./AnimatedSection"
 
 const containerVariants = {
   hidden: { opacity: 0, y: 50 },
@@ -44,66 +43,39 @@ const HowItWorksSection = () => {
           </Heading>
         </motion.div>
         <div className="flex flex-col w-full sm:w-1/2 gap-6">
-          <div className="relative w-full sm:h-72">
-            <motion.div
-              className="relative sm:absolute sm:top-10 sm:-left-16"
-              variants={itemVariants}
-            >
-              <Image src={darkSilverShapeImage} alt="Dark silver shape" />
-              <div className="absolute top-0 left-10 w-full h-full flex items-center justify-center">
-                <div className="flex items-center gap-4">
-                  <Image
-                    src={spotifyLockImage}
-                    alt="Spotify lock"
-                    className="h-40 w-auto"
-                  />
-                  <Heading as="h3" size="xl" color="dark" weight="normal">
-                    Sign in on your Spotify account
-                  </Heading>
-                </div>
-              </div>
-            </motion.div>
-          </div>
-          <div className="relative w-full sm:h-72">
-            <motion.div
-              className="relative sm:absolute sm:-top-24 sm:-right-28"
-              variants={itemVariants}
-            >
-              <Image src={platinumShapeImage} alt="Dark silver shape" />
-              <div className="absolute top-0 left-10 h-full flex items-center justify-center">
-                <div className="flex items-center gap-4">
-                  <Image
-                    src={searchArtistsImage}
-                    alt="Search artists"
-                    className="h-40 w-auto"
-                  />
-                  <Heading as="h3" size="xl" color="neutral" weight="normal">
-                    Find and select the artists using Spotify search engine
-                  </Heading>
-                </div>
-              </div>
-            </motion.div>
-          </div>
-          <div className="relative w-full sm:h-72">
-            <motion.div
-              className="relative sm:absolute sm:-top-16 sm:-left-16"
-              variants={itemVariants}
-            >
-              <Image src={darkSilverShapeImage} alt="Dark silver shape" />
-              <div className="absolute top-0 left-10 w-full h-full flex items-center justify-center">
-                <div className="flex items-center gap-4">
-                  <Image
-                    src={customizePlaylistImage}
-                    alt="Customize playlist"
-                    className="h-40 w-auto"
-                  />
-                  <Heading as="h3" size="xl" color="dark" weight="normal">
-                    Check the generated playlist and customize it as you like
-                  </Heading>
-                </div>
-              </div>
-            </motion.div>
-          </div>
+          <AnimatedSection
+            backgroundShape="platinum"
+            floatingPosition="sm:top-10 sm:-left-16"
+          >
+            <AnimatedSection.Image src={spotifyLockImage} alt="Spotify lock" />
+            <AnimatedSection.Title>
+              Sign in on your Spotify account
+            </AnimatedSection.Title>
+          </AnimatedSection>
+          <AnimatedSection
+            backgroundShape="dark-silver"
+            floatingPosition="sm:-top-0 sm:-right-28"
+          >
+            <AnimatedSection.Image
+              src={searchArtistsImage}
+              alt="Search artists"
+            />
+            <AnimatedSection.Title>
+              Find and select the artists using Spotify search engine
+            </AnimatedSection.Title>
+          </AnimatedSection>
+          <AnimatedSection
+            backgroundShape="platinum"
+            floatingPosition="sm:-top-16 sm:-left-16"
+          >
+            <AnimatedSection.Image
+              src={customizePlaylistImage}
+              alt="Customize playlist"
+            />
+            <AnimatedSection.Title>
+              Check the generated playlist and customize it as you like
+            </AnimatedSection.Title>
+          </AnimatedSection>
         </div>
       </motion.div>
     </section>
