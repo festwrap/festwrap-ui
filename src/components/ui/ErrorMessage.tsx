877e762fc19af619ea4ec60a626039d44f5bf98a
alex.barro <a.barro.sendros@gmail.com>
2024-12-06 16:54:31 +0100

    chore(ui): create ErrorMessage component

---
+++ src/components/ui/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <p className="text-xs text-red-600 dark:text-red-400" role="alert">
+      {children}
+    </p>
+  )
+}
+
+export default ErrorMessage
