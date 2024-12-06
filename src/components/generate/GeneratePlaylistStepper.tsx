13879feb9c6747ae5a2715d2f4157cad1b233391
alex.barro <a.barro.sendros@gmail.com>
2024-12-06 11:56:05 +0100

    feat: change steps navigation using form validations

--- src/components/generate/GeneratePlaylistStepper.tsx
+++ src/components/generate/GeneratePlaylistStepper.tsx
@@ -9,8 +9,10 @@ import { Stepper, StepList, Step, StepContent } from "@components/ui/Stepper"
 import useTranslation from "next-translate/useTranslation"
 import Link from "next/link"
 import { useState } from "react"
+import { Form } from "@components/ui/Form"
 
 const STEPS_COUNT = 3
+
 const formSchema = z
   .object({
     playlistType: z.enum(["new", "existing"]),
@@ -42,6 +44,7 @@ export type FormSchemaType = z.infer<typeof formSchema>
 const GeneratePlaylistStepper = () => {
   const { t } = useTranslation("generate")
   const [currentStep, setCurrentStep] = useState(1)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -53,74 +56,88 @@ const GeneratePlaylistStepper = () => {
     },
   })
 
+  const { handleSubmit, trigger } = form
+
+  const handleNext = async () => {
+    const isStepValid = await trigger(["name", "isPrivate", "playlistType"])
+    if (isStepValid) setCurrentStep((prev) => prev + 1)
+  }
+
+  const handleBack = () => setCurrentStep((prev) => prev - 1)
+
+  const onSubmit = (values: FormSchemaType) => {
+    setCurrentStep((prev) => prev + 1)
+    console.log(values)
+  }
+
   const handleChangeStep = (step: number) => {
     setCurrentStep(step)
   }
 
-  const handleNext = () => {
-    setCurrentStep((prev) => prev + 1)
-  }
-
-  const handleBack = () => {
-    setCurrentStep((prev) => prev - 1)
-  }
-
   const shouldDisplayBackButton = currentStep > 1 && currentStep !== STEPS_COUNT
-  const shouldDisplayNextButton = currentStep < STEPS_COUNT
+  const shouldDisplayNextButton = currentStep === 1
   const shouldDisplayFinishButton = currentStep === STEPS_COUNT
+  const shouldDisplaySubmitButton = currentStep === 2
 
   return (
-    <div className="space-y-6 flex">
-      <Stepper
-        stepsCount={STEPS_COUNT}
-        currentStep={currentStep}
-        onStepChange={handleChangeStep}
-      >
-        <StepList>
-          <Step
-            stepNumber={1}
-            title={t("steps.step1.title")}
-            description={t("steps.step1.description")}
-          />
-          <Step
-            stepNumber={2}
-            title={t("steps.step2.title")}
-            description={t("steps.step2.description")}
-          />
-          <Step
-            stepNumber={3}
-            title={t("steps.step3.title")}
-            description={t("steps.step3.description")}
-          />
-        </StepList>
-        <div className="min-h-[450px] flex flex-1 flex-col justify-between">
-          <StepContent stepNumber={1}>
-            <PlaylistSetupForm />
-          </StepContent>
-          <StepContent stepNumber={2}>
-            <PlaylistSearchBandsForm />
-          </StepContent>
-          <StepContent stepNumber={3}>
-            <PlaylistGetUrlLink />
-          </StepContent>
-          <div className="flex justify-end space-x-6 mt-8">
-            {shouldDisplayBackButton && (
-              <Button variant="ghost" onClick={handleBack}>
-                {t("steps.navigation.previous")}
-              </Button>
-            )}
-            {shouldDisplayNextButton && (
-              <Button onClick={handleNext}>{t("steps.navigation.next")}</Button>
-            )}
-            {shouldDisplayFinishButton && (
-              <Button asChild>
-                <Link href="/">{t("steps.navigation.finish")}</Link>
-              </Button>
-            )}
+    <Form {...form}>
+      <form className="space-y-6 flex" onSubmit={handleSubmit(onSubmit)}>
+        <Stepper
+          stepsCount={STEPS_COUNT}
+          currentStep={currentStep}
+          onStepChange={handleChangeStep}
+        >
+          <StepList>
+            <Step
+              stepNumber={1}
+              title={t("steps.step1.title")}
+              description={t("steps.step1.description")}
+            />
+            <Step
+              stepNumber={2}
+              title={t("steps.step2.title")}
+              description={t("steps.step2.description")}
+            />
+            <Step
+              stepNumber={3}
+              title={t("steps.step3.title")}
+              description={t("steps.step3.description")}
+            />
+          </StepList>
+          <div className="min-h-[450px] flex flex-1 flex-col justify-between">
+            <StepContent stepNumber={1}>
+              <PlaylistSetupForm />
+            </StepContent>
+            <StepContent stepNumber={2}>
+              <PlaylistSearchBandsForm />
+            </StepContent>
+            <StepContent stepNumber={3}>
+              <PlaylistGetUrlLink />
+            </StepContent>
+            <div className="flex justify-end space-x-6 mt-8">
+              {shouldDisplayBackButton && (
+                <Button variant="ghost" onClick={handleBack}>
+                  {t("steps.navigation.previous")}
+                </Button>
+              )}
+              {shouldDisplayNextButton && (
+                <Button onClick={handleNext}>
+                  {t("steps.navigation.next")}
+                </Button>
+              )}
+              {shouldDisplaySubmitButton && (
+                <Button type="submit">{t("steps.navigation.generate")}</Button>
+              )}
+              {shouldDisplayFinishButton && (
+                <Button asChild>
+                  <Link href="/">{t("steps.navigation.finish")}</Link>
+                </Button>
+              )}
+            </div>
           </div>
-        </div>
-      </Stepper>
-    </div>
+        </Stepper>
+      </form>
+    </Form>
   )
 }
 
