2323b5197efff00b2393d24c698a557376321ff6
alex.barro <a.barro.sendros@gmail.com>
2024-12-26 03:41:22 +0100

    refactor: use Heading instead of html element directly

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import Heading from "@/components/ui/Heading"
 import { Button } from "@components/ui/Button"
 import useTranslation from "next-translate/useTranslation"
 import Image from "next/image"
@@ -15,7 +16,7 @@ export default function Custom404() {
         className="mx-auto"
       />
       <div className="flex flex-col gap-3 justify-center items-center">
-        <h1 className="text-3xl text-dark font-medium text-center">404</h1>
+        <Heading size="2xl">404</Heading>
         <p className="text-light text-center">{t("notFound.message")}</p>
         <Button asChild variant="ghost">
           <Link href="/">{t("notFound.button")}</Link>
