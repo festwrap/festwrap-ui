0c91286f3da967d5d12a34748862a28e4236276d
Dani <daniel.mora.checa@gmail.com>
2025-01-05 09:53:40 +0100

    chore(http): add http base client

---
+++ src/lib/clients/http.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export enum Method {
+  Get = 'GET', // eslint-disable-line no-unused-vars
+}
+
+export interface HttpRequest {
+  url: string;
+  method: Method;
+  params?: Record<string, any>;
+  headers?: Record<string, string>;
+}
+
+export interface HttpClient {
+  send(_request: HttpRequest): Promise<HttpResponse>;
+}
+
+export interface HttpResponse {
+  data: any;
+  status: number;
+}
+
+export class HttpBaseClient implements HttpClient {
+  async send({
+    url,
+    method,
+    params,
+    headers,
+  }: HttpRequest): Promise<HttpResponse> {
+    return axios({
+      url,
+      method,
+      params,
+      headers,
+    }).then((response) => {
+      return {
+        data: response.data,
+        status: response.status,
+      };
+    });
+  }
+}
