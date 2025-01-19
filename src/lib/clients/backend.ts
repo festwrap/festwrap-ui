02e31d617bf811a9fa472c3097c7decccf8989a9
Dani <daniel.mora.checa@gmail.com>
2025-01-19 19:09:16 +0100

    chore(backend): add fake client

--- src/lib/clients/backend.ts
+++ src/lib/clients/backend.ts
@@ -3,7 +3,11 @@ import { AuthClient } from './auth';
 import { HttpClient, Method } from './http';
 
 export interface BackendClient {
-  searchArtists(_token: string, _name: string, _limit: number): Promise<Artist[]>;
+  searchArtists(
+    _token: string,
+    _name: string,
+    _limit: number
+  ): Promise<Artist[]>;
 }
 
 export class HTTPBackendClient implements BackendClient {
@@ -47,5 +51,29 @@ export class HTTPBackendClient implements BackendClient {
     return {
       [this.authClient.getHeaderName()]: `Bearer ${authToken}`,
     };
+  }
+}
+
+export class FakeBackendClient implements BackendClient {
+  private searchArtistError: Error | undefined = undefined;
+  private searchArtistResult: Artist[];
+
+  constructor(result: Artist[] = []) {
+    this.searchArtistResult = result;
+  }
+
+  setResult(result: Artist[]) {
+    this.searchArtistResult = result;
+  }
+
+  setSearchArtistError(error: Error) {
+    this.searchArtistError = error;
+  }
+
+  async searchArtists(..._: any[]): Promise<Artist[]> {
+    if (this.searchArtistError !== undefined) {
+      throw this.searchArtistError;
+    }
+    return this.searchArtistResult;
   }
 }
