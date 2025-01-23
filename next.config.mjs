3f703d2e001f1763c5aa6fbcd05893a25ee577ac
Dani <daniel.mora.checa@gmail.com>
2025-01-24 00:05:47 +0100

    chore(config): allow downloading Spotify images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import nextTranslate from "next-translate-plugin"
 
 const nextConfig = nextTranslate({
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'i.scdn.co',
+      port: '',
+      pathname: '/image/**'
+    }]
+  },
 })
 
 export default nextConfig
