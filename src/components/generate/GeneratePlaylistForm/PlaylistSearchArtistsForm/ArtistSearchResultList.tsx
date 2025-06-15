bf7a05bdfd5dad2a042d51cd6e1399b59e2e717e
Dani <daniel.mora.checa@gmail.com>
2025-06-15 18:04:31 +0200

    refactor: improve search artist readability

--- src/components/generate/GeneratePlaylistForm/PlaylistSearchArtistsForm/ArtistSearchResultList.tsx
+++ src/components/generate/GeneratePlaylistForm/PlaylistSearchArtistsForm/ArtistSearchResultList.tsx
@@ -1,6 +1,7 @@
 import { ArtistDTO } from '@/entities/artists';
 import ArtistSearchResult from './ArtistSearchResult';
 import useTranslation from 'next-translate/useTranslation';
+import { cn } from '@/lib/utils';
 
 /* eslint-disable no-unused-vars */
 export enum ArtistSearchStatus {
@@ -31,7 +32,7 @@ export function ArtistSearchResultList({
   function buildMessageItem(text: string, role: 'status' | 'alert') {
     const textClass = role === 'status' ? 'text-secondary' : 'text-red-400';
     return (
-      <li className={`px-4 py-2 ${textClass}`} role={role}>
+      <li className={cn(`px-4 py-2 ${textClass}`)} role={role}>
         {text}
       </li>
     );
@@ -45,38 +46,45 @@ export function ArtistSearchResultList({
     return buildMessageItem(text, 'alert');
   }
 
+  function renderSearchItems() {
+    switch (status) {
+      case ArtistSearchStatus.Searching:
+        return buildStatusItem(
+          t('playlistSearchArtists.artistSearch.searching')
+        );
+      case ArtistSearchStatus.Empty:
+        return buildStatusItem(t('playlistSearchArtists.artistSearch.empty'));
+      case ArtistSearchStatus.Error:
+        return buildAlertStatusItem(t('errors.artistSearch.unexpectedError'));
+      case ArtistSearchStatus.NoResults:
+        return buildStatusItem(
+          t('playlistSearchArtists.artistSearch.noResults')
+        );
+      case ArtistSearchStatus.HasResults:
+        return searchedArtists.map((item, index) => (
+          <ArtistSearchResult
+            key={index}
+            name={item.name}
+            isActive={index === activeArtistIndex}
+            isSelected={selectedArtists.some(
+              (selectedItem) => selectedItem.name === item.name
+            )}
+            handleItemSelect={() => onArtistSelect(item)}
+            imageUrl={item.imageUri}
+          />
+        ));
+      default:
+        return buildAlertStatusItem(t('errors.artistSearch.unexpectedError'));
+    }
+  }
+
   return (
     <ul
       id="combobox-items"
       role="listbox"
       className="absolute z-10 w-full mt-2 bg-white border border-secondary rounded-xl shadow-lg max-h-60 overflow-auto py-3"
     >
-      {status === ArtistSearchStatus.Searching
-        ? buildStatusItem(t('playlistSearchArtists.artistSearch.searching'))
-        : status === ArtistSearchStatus.Empty
-          ? buildStatusItem(t('playlistSearchArtists.artistSearch.empty'))
-          : status === ArtistSearchStatus.Error
-            ? buildAlertStatusItem(t('errors.artistSearch.unexpectedError'))
-            : status === ArtistSearchStatus.NoResults
-              ? buildStatusItem(
-                  t('playlistSearchArtists.artistSearch.noResults')
-                )
-              : status === ArtistSearchStatus.HasResults
-                ? searchedArtists.map((item, index) => (
-                    <ArtistSearchResult
-                      key={index}
-                      name={item.name}
-                      isActive={index === activeArtistIndex}
-                      isSelected={selectedArtists.some(
-                        (selectedItem) => selectedItem.name === item.name
-                      )}
-                      handleItemSelect={() => onArtistSelect(item)}
-                      imageUrl={item.imageUri}
-                    />
-                  ))
-                : buildAlertStatusItem(
-                    t('errors.artistSearch.unexpectedError')
-                  )}
+      {renderSearchItems()}
     </ul>
   );
 }
